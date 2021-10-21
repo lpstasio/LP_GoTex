7d068aa0643a581f8e9b02cd6af83a3f4892b9e4
lpstasio <dave.stasio1@outlook.com>
2021-10-21 16:25:19 +0200

    reading and writing dirs

--- tex.go
+++ tex.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import ("fmt"; "os")
+
+var VERSION = 0;
+
+func file_exists(path string) bool {
+    result := false;
+    if _, er := os.Stat(path); er == nil {
+        result = true;
+    }
+    return result;
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Printf("TEXporter v%v\n", VERSION);
+
+    if !file_exists("in") {
+        os.Mkdir("in", os.ModeDir)
+    }
+    if !file_exists("out") {
+        os.Mkdir("out", os.ModeDir)
+    }
+    if !file_exists("config") {
+        os.Mkdir("config", os.ModeDir)
+    }
+
+    paths, _ := os.ReadDir("in");
+
+    for it_index, it := range paths {
+        fmt.Println(it_index, it.Name(), it.IsDir());
+    }
 }
